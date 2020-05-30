3090fc4cc17a1dccee381cb4a5abe5294488f87b
Alexandre Djioev <adjioev@solutiondigital.biz>
2020-05-30 16:15:32 +0400

    Footer prototype

--- footer.php
+++ footer.php
@@ -1,6 +1,38 @@
 			<!-- footer -->
-			<footer class="footer" role="contentinfo"></footer>
-			<!-- /footer -->
+			<footer class="footer" role="contentinfo">
+					<div class="footer-question">
+						<h5>What would you like to do?</h5>
+					</div>
+					<div class="footer-menu">
+						<div>Sell</div>
+						<div>Buy</div>
+						<div>Manage</div>
+						<div>Rent</div>
+					</div>
+
+					<div class="footer-logo">
+						<a href="<?php echo home_url(); ?>">
+							<img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/img/WWlogo_tranparent.png" alt="Logo">
+						</a>
+						<a href="<?php echo home_url(); ?>">
+							<img class="logo-text" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
+						</a>
+					</div>
+					<div class="contact-info">
+						<div>t. 02 8383 3333</div>
+						<div>f. 02 8383 3383</div>
+						<div>e. info@wentworthpartners.com.au</div>
+					</div>
+					<div class="head-office-address">
+						<h6><strong>Head Office</strong></h6>
+						<h6>31 Belmore Road Randwick NSW 2031</h6>
+					</div>
+					<div class="copyright">
+						<h6>Wentworth Partners Management ABN 82 627 821 489</h6>
+						<h6>Trading at Wentworth Partners</h6>
+					</div>
+			</footer>
+				<!-- /footer -->
 
 		</div>
 		<!-- /wrapper -->
