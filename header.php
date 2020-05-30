f7de8948803005f1bd943193d28464b0231bc56b
Alexandre Djioev <adjioev@solutiondigital.biz>
2020-05-30 16:15:18 +0400

    Updated logo

--- header.php
+++ header.php
@@ -30,13 +30,14 @@
 
 			<!-- header -->
 			<header class="header clear" role="banner">
-
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
+							<img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/img/WWlogo_tranparent.png" alt="Logo">
 						</a>
+						<a href="<?php echo home_url(); ?>">
+							<img class="logo-text" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
+						</a>						
 					</div>
 					<!-- /logo -->
 
