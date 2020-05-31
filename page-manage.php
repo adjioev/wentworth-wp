4b5f434825acafd212e4f7bcded1a2820bbf870d
Alexandre Djioev <adjioev@solutiondigital.biz>
2020-05-31 17:49:12 +0400

    Some progress with sell and manage pages

--- page-manage.php
+++ page-manage.php
@@ -1,25 +1,45 @@
-<div class="front-page-container">
+<div class="manage-page">
 	<?php get_header(); ?>
 
 	<main>
-		<?php if (has_post_thumbnail( $post->ID ) ): ?>
-			<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-			<div id="front-page-bg" style="background-image: url('<?php echo $image[0]; ?>')">
-			</div>
-		<?php endif; ?>
+		<div class="hero-image-block">
+			<?php if (has_post_thumbnail( $post->ID ) ): ?>
+				<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+					<div class="hero-content-block" style="background-image: url('<?php echo $image[0]; ?>')">
+						<div class="title"><h1>Manage Your Property With Us</h1></div>
 
-		<div class="front-page-title">
-			<?php 
-				the_post();
-				the_content();
-			?>
+
+						<div class="content-buttons">
+							<div class="wp-block-buttons aligncenter">
+								<div class="wp-block-button is-style-outline">
+									<a class="wp-block-button__link has-text-color has-very-light-gray-color no-border-radius">Download our<br>Property Management Guide</a>
+								</div>
+							</div>
+
+							<div class="wp-block-buttons aligncenter">
+								<div class="wp-block-button is-style-outline">
+									<a class="wp-block-button__link has-text-color has-very-light-gray-color no-border-radius">Request a<br>Property Appraisal</a>
+								</div>
+							</div>
+						</div>
+				</div>
+			<?php endif; ?>
 		</div>
-		<div class="front-page-agents">
-		<?php echo do_shortcode('[agents_carousel position="property manager"]'); ?>
 
+		<div class="page-content container">
+				<?php 
+					if ( have_posts() ) {
+						while ( have_posts() ) {
+							the_post(); 
+							the_content();
+						}
+					}
+				?>
 		</div>
 	</main>
 
-	<?php #get_footer(); ?>
+	<div class="front-page-agents">
+		<?php echo do_shortcode('[agents_carousel position="property manager"]'); ?>
+	</div>
 </div>
 
--- page-sell.php
+++ page-sell.php
@@ -1,25 +1,46 @@
-<div class="front-page-container">
+<div class="sell-page">
 	<?php get_header(); ?>
 
 	<main>
-		<?php if (has_post_thumbnail( $post->ID ) ): ?>
-			<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-			<div id="front-page-bg" style="background-image: url('<?php echo $image[0]; ?>')">
-			</div>
-		<?php endif; ?>
+		<div class="hero-image-block">
+			<?php if (has_post_thumbnail( $post->ID ) ): ?>
+				<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+					<div class="hero-content-block" style="background-image: url('<?php echo $image[0]; ?>')">
+						<div class="title"><h1>Sell Your Property With Us</h1></div>
 
-		<div class="front-page-title">
-			<?php 
-				the_post();
-				the_content();
-			?>
-		</div>
-		<div class="front-page-agents">
-		<?php echo do_shortcode('[agents_carousel position="sales agent"]'); ?>
 
+						<div class="content-buttons">
+							<div class="wp-block-buttons aligncenter">
+								<div class="wp-block-button is-style-outline">
+									<a class="wp-block-button__link has-text-color has-very-light-gray-color no-border-radius">Download our<br>Guide to selling your home</a>
+								</div>
+							</div>
+
+							<div class="wp-block-buttons aligncenter">
+								<div class="wp-block-button is-style-outline">
+									<a class="wp-block-button__link has-text-color has-very-light-gray-color no-border-radius">Request a<br>Property Appraisal</a>
+								</div>
+							</div>
+						</div>
+				</div>
+			<?php endif; ?>
 		</div>
+
+		<div class="page-content container">
+				<?php 
+					if ( have_posts() ) {
+						while ( have_posts() ) {
+							the_post(); 
+							the_content();
+						}
+					}
+				?>
+		</div>
+
 	</main>
 
-	<?php #get_footer(); ?>
+	<div class="front-page-agents">
+		<?php echo do_shortcode('[agents_carousel position="sales agent"]'); ?>
+	</div>
 </div>
 
